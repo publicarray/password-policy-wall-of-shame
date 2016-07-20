33f768269e801eb62962f7a651e15c382ae3534c
Sebastian Schmidt <publicarray@icloud.com>
2016-07-20 13:05:16 +1000

    improve ajax error message.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
 ---
 ---
-// $.getJSON("{{site.baseurl}}/search.json", processJson());
 $.ajax({
     dataType: "json",
     url: '{{site.baseurl}}/search.json',
     success: processJson()
-}).done(function(jqXHR) {
-    console.log("second success", jqXHR.status);
-  })
-  .fail(function(jqXHR) {
-    console.log("error", jqXHR.status);
-  })
-  .always(function(jqXHR) {
-    console.log("complete", jqXHR.status);
-  });
+}).fail(function showAjaxErrorMessage(jqXHR) {
+        var status = jqXHR.status + ' (' + jqXHR.statusText + ')';
+        console.error('Unable to retrieve or parse \"'+this.url+'\" as '+this.dataType+'. [Search is disabled]', status);
+        $('.ui.search').removeClass('loading');
+        $('.ui.search .icon').removeClass('link');
+        $('.ui.search .input').addClass('disabled');
+    });
 
 function processJson() {
     return function (data) {
-        console.log(data);
         var searchBar = $('.ui.search');
         searchBar.search({
             source: data,
